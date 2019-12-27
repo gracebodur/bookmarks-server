bdec69f17d04b3efae4593d86e4bb47486f7e85c
Grace <mgracebodur@gmail.com>
2019-12-27 03:08:56 -0500

    DELETE route created, need to be tested in postman

--- src/bookmarks/bookmark-router.js
+++ src/bookmarks/bookmark-router.js
@@ -17,7 +17,9 @@ bookmarkRouter
 
         if(!title || !url || !desc) {
             logger.error(`Title, url and description are required`)
-            return res.status(400).send('Title, url and description are required')
+            return res
+                .status(400)
+                .send('Title, url and description are required')
         }
 
         if(rating < 0 || rating > 5 || !Number.isInteger(rating)) {
@@ -26,7 +28,9 @@ bookmarkRouter
 
         if(!validUrl.isWebUri(url)) {
             logger.error(`Url is invalid`)
-            return res.status(400).send('Url must be formed as HTTP or HTTPS')
+            return res
+                .status(400)
+                .send('Url must be formed as HTTP or HTTPS')
         }
 
         const id = uuid()
@@ -42,7 +46,32 @@ bookmarkRouter
         
         logger.info(`Bookmark with id: ${id} created`)
 
-        res.status(201).location(`http://localhost:8000/bookmarks/${id}`).json(bookmark)
+        res
+            .status(201)
+            .location(`http://localhost:8000/bookmarks/${id}`)
+            .json(bookmark)
+    })
+    .delete((req, res) => {
+        const { id } = req.params
+
+        const bookmarkIndex = bookmarks.findIndex(b => b.id == id)
+
+        if(bookmarkIndex === -1) {
+            logger.error(`Bookmark with id: ${id} not found.`)
+            return res
+                .status(400)
+                .send('Not found')
+        }
+
+
+
+        bookmarks.splice(bookmarkIndex, 1)
+
+        logger.info(`Bookmark with id: ${id} deleted.`)
+
+        res
+        .status(204)
+        .end()
     })
 
     module.exports = bookmarkRouter
