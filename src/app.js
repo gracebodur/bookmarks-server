be7bad4f87e64274076d777e0f0e55ed72228647
Grace <mgracebodur@gmail.com>
2019-12-27 01:12:30 -0500

    Configured logging and API key handling middleware

--- src/app.js
+++ src/app.js
@@ -5,19 +5,19 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
+//create an Express application
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common'
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common'
+
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-app.use(helmet())
+
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+    res.send('Hello, Grace!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
