1e66b128471cab30aeda457900f530f03cf7bfb5
Grace <mgracebodur@gmail.com>
2019-12-27 02:28:09 -0500

    POST bookmarks router postman tested,  working

--- src/bookmarks/bookmark-router.js
+++ src/bookmarks/bookmark-router.js
@@ -12,10 +12,37 @@ bookmarkRouter
     .get((req, res) => {
         res.json(bookmarks)
     })
-    // .post(bodyParser, (req, res) => {
-    //     const { title, url, rating = 1, desc } = req.body
+    .post(bodyParser, (req, res) => {
+        const { title, url, rating = 1, desc } = req.body
 
+        if(!title || !url || !desc) {
+            logger.error(`Title, url and description are required`)
+            return res.status(400).send('Title, url and description are required')
+        }
 
-    // })
+        if(rating < 0 || rating > 5 || !Number.isInteger(rating)) {
+            logger.error(`Rating must be a number between 0 and 5`)
+        }
+
+        if(!validUrl.isWebUri(url)) {
+            logger.error(`Url is invalid`)
+            return res.status(400).send('Url must be formed as HTTP or HTTPS')
+        }
+
+        const id = uuid()
+        const bookmark = {
+            id,
+            title,
+            url,
+            rating,
+            desc
+        }
+
+        bookmarks.push(bookmark)
+        
+        logger.info(`Bookmark with id: ${id} created`)
+
+        res.status(201).location(`http://localhost:8000/bookmarks/${id}`).json(bookmark)
+    })
 
     module.exports = bookmarkRouter
